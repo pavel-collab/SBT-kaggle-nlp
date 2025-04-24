0e2be483ac3e9d1fa34413206fff69cefbeaad01
Filippenko Pavel <filippenko.ps@phystech.edu>
2025-04-25 00:47:40 +0300

    two successful generation models

--- src/generation/generation.py
+++ src/generation/generation.py
@@ -1,35 +1,49 @@
-import openai
+from vllm import LLM, SamplingParams
 from transformers import pipeline
-import os
+import gc
+import torch
 
-openai.api_key = os.environ.get('OPENAI_TOKEN')
+# Загружаем LLM
+# llm = LLM(model='Qwen/Qwen2.5-0.5B-Instruct')
+llm = LLM(model='deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B',
+          tokenizer_mode="auto",
+          dtype="auto",
+          gpu_memory_utilization=0.9,
+          max_num_seqs=128,
+          max_model_len=2048)
 
 target_label = "Linear Algebra"
 labels = ["Algebra", "Geometry and Trigonometry", "Calculus and Analysis",
-                "Probability and Statistics", "Number Theory", "Combinatorics and Discrete Math",
-                "Linear Algebra", "Abstract Algebra and Topology"]
+          "Probability and Statistics", "Number Theory", "Combinatorics and Discrete Math",
+          "Linear Algebra", "Abstract Algebra and Topology"]
 
 # Промпт с уточнением стиля
 prompt = f"""Generate an example of a short piece of mathematical text, which is {target_label} problem.
-Don't use general phrases, give a strict, formal definition of a mathematical object."""
+Don't use general phrases, give an example of mathematical problem that teacher can give in school or univercity."""
 
-def generate_text(prompt):
-    response = openai.ChatCompletion.create(
-        model="qwen/qwen2.5-coder-7b-instruct",
-        messages=[{"role": "user", "content": prompt}],
-        temperature=0.5,
-        max_tokens=150
-    )
-    return response.choices[0].message['content'].strip()
+sampling_params = SamplingParams(
+            temperature=0.9, top_k=500, top_p=0.9, max_tokens=512, n=1
+        )
 
-# Классификатор-судья (можно заменить на свою модель)
-#! here we can put a pretrained by us basic model
-classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
-generated = generate_text(prompt)
+# Генерация
+outputs = llm.generate(prompt, sampling_params)
+generated = outputs[0].outputs[0].text.strip()
+
+print("\nGenerated text:")
+print(generated)
+
+# освободим память перед применением новой нейронки
+gc.collect()
+torch.cuda.empty_cache()
+
+# Проверка классификатором
+#! мы можем использовать здесь предобученную нами модель
+#! ради экономии видеопамяти эту часть можно вынести в отдельный скрипт
+#! мы можем попробовать здесь другие модели
+classifier = pipeline("zero-shot-classification", model="distilbert-base-uncased")
 result = classifier(generated, labels)
 
-print(f"\nGenerated text:\n{generated}")
-print(f"\nClassified as: {result['labels'][0]} (score={result['scores'][0]:.2f})")
+print(f"\nClassification: {result['labels'][0]} (score={result['scores'][0]:.2f})")
 
 if result['labels'][0] == target_label and result['scores'][0] > 0.7:
     print("Example accepted")
