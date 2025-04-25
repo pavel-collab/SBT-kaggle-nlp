ca3bd50a4fdc1111ff0fb8622d27db5458e70600
Filippenko Pavel <filippenko.ps@phystech.edu>
2025-04-25 21:59:59 +0300

    base prodaction implementation of generation and augmentation

--- src/generation/augmentation.py
+++ src/generation/augmentation.py
@@ -1,27 +1,71 @@
 from transformers import pipeline
 import pandas as pd
+import argparse
+from pathlib import Path
 
-paraphraser = pipeline("text2text-generation", 
-                       model="ramsrigouthamg/t5_paraphraser", 
-                       tokenizer="t5-base")
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', '--target_label', type=str, default="Linear Algebra", help='targer class that nn will generate')
+parser.add_argument('-d', '--data_path', help='path to train data')
+parser.add_argument('-n', '--n_samples', type=int, default=10, help='number of generated samples')
+parser.add_argument('-o', '--output_path', type=str, default='./data/', help='path to directory where we will save output file with generated data')
+args = parser.parse_args()
 
-prompt = "paraphrase: {text} </s>"
+target_label = args.target_label
 
-df = pd.read_csv('../../data/train.csv')
+label2idx ={
+    "Algebra": 0, 
+    "Geometry and Trigonometry": 1, 
+    "Calculus and Analysis": 2,
+    "Probability and Statistics": 3, 
+    "Number Theory": 4, 
+    "Combinatorics and Discrete Math": 5,
+    "Linear Algebra": 6, 
+    "Abstract Algebra and Topology": 7
+}
 
-for idx, row in df.iterrows():
-    outputs = paraphraser(prompt.format(text = row['Question']), 
-                          max_length=128, 
-                          num_return_sequences=3, 
-                          do_sample=True,
-                          temperature=1.1,
-                          top_k=500,
-                          top_p=0.95)
+def main():
+
+    if args.data_path is None or args.data_path == "":
+        print("ERROR you didn't set up data file path")
+        return
     
-    print(f"Original text {row['Question']}")
-    print("Perephrased outputs:")
-    for i, out in enumerate(outputs, 1):
-        print(f"{i}) {out['generated_text']}")
-    print()
-    
-    if idx == 5: break
+    data_path = Path(args.data_path)
+    assert(data_path.exists())
+
+    paraphraser = pipeline("text2text-generation", 
+                        model="ramsrigouthamg/t5_paraphraser", 
+                        tokenizer="t5-base")
+
+    prompt = "paraphrase: {text} </s>"
+
+    df = pd.read_csv(data_path.absolute())
+
+    for idx, row in df.iterrows():
+        if idx == args.n_samples:
+            break
+        
+        outputs = paraphraser(prompt.format(text = row['Question']), 
+                            max_length=128, 
+                            num_return_sequences=1, #! number of perefrase generations 
+                            do_sample=True,
+                            temperature=1.1,
+                            top_k=500,
+                            top_p=0.95)
+        
+        output_file_path = Path(args.output_path)
+        output_file = Path(f"{output_file_path.absolute()}/{target_label.replace(' ', '-')}_augmented_samples.csv")
+        file_create = output_file.exists()
+
+        for out in outputs:
+            generated = out['generated_text']
+            
+            # clean result before insert in file
+            cleaned_generation = generated.replace(',', '').replace('\n', ' ')
+            
+            with open(output_file.absolute(), 'a') as fd:
+                if not file_create:
+                    fd.write("Question,label\n")
+                fd.write(f"{cleaned_generation},{label2idx[target_label]}\n")
+        
+if __name__ == '__main__':
+    main()
--- src/generation/generation.py
+++ src/generation/generation.py
@@ -3,12 +3,16 @@ from transformers import pipeline
 import gc
 import torch
 import argparse
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer
-import numpy as np
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch.nn.functional as F
+from pathlib import Path
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--pipeline', action='store_true', help='use raw pipeline to classify generationed result')
+parser.add_argument('-m', '--model_path', help='set a path to the model that we want to evaluate')
+parser.add_argument('-t', '--target_label', type=str, default="Linear Algebra", help='targer class that nn will generate')
+parser.add_argument('-n', '--n_samples', type=int, default=10, help='number of generated samples')
+parser.add_argument('-o', '--output_path', type=str, default='./data/', help='path to directory where we will save output file with generated data')
 args = parser.parse_args()
 
 # Загружаем LLM
@@ -20,11 +24,13 @@ llm = LLM(model='Qwen/Qwen2.5-0.5B-Instruct')
 #           max_num_seqs=128,
 #           max_model_len=2048)
 
-target_label = "Linear Algebra"
+target_label = args.target_label
+n_samples = args.n_samples
+
+#TODO: move to constants file
 labels = ["Algebra", "Geometry and Trigonometry", "Calculus and Analysis",
           "Probability and Statistics", "Number Theory", "Combinatorics and Discrete Math",
           "Linear Algebra", "Abstract Algebra and Topology"]
-
 idx2labels ={
     0: "Algebra", 
     1: "Geometry and Trigonometry", 
@@ -41,52 +47,75 @@ prompt = f"""Generate an example of a short piece of mathematical text, which is
 Don't use general phrases, give an example of mathematical problem that teacher can give in school or univercity."""
 
 sampling_params = SamplingParams(
-            temperature=0.9, top_k=500, top_p=0.9, max_tokens=512, n=1
+            temperature=0.9, top_k=500, top_p=0.9, max_tokens=512, n=n_samples
         )
 
 # Генерация
 outputs = llm.generate(prompt, sampling_params)
-generated = outputs[0].outputs[0].text.strip()
 
-print("\nGenerated text:")
-print(generated)
+accepted_samples_number = 0
+for output in outputs[0].outputs:
+    #TODO полная строка генерации outputs[0].outputs[0] -- разобраться что значат эти индексы
+    #TODO в генерациях остаются артефакты генерации в начале предложения, придумать, как убрать их
+    generated = output.text.strip()
 
-# освободим память перед применением новой нейронки
-gc.collect()
-torch.cuda.empty_cache()
+    # освободим память перед применением новой нейронки
+    gc.collect()
+    torch.cuda.empty_cache()
 
-# Проверка классификатором
-#! ради экономии видеопамяти эту часть можно вынести в отдельный скрипт
-#! мы можем попробовать здесь другие модели
-if args.pipeline:
-    classifier = pipeline("zero-shot-classification", model="distilbert-base-uncased")
-    result_info = classifier(generated, labels)
-    score = result_info['scores'][0]
-    result = result_info['labels'][0]
-else:
-    tokenizer_file_path = '../../results/bert-base-uncased_results/tokenizer'
-    model_file_path = '../../results/bert-base-uncased_results/checkpoint-5000'
-    tokenizer = AutoTokenizer.from_pretrained(tokenizer_file_path)
-    # Загрузка модели
-    model = AutoModelForSequenceClassification.from_pretrained(model_file_path)
+    # Проверка классификатором
+    #! ради экономии видеопамяти эту часть можно вынести в отдельный скрипт
+    #! мы можем попробовать здесь другие модели
+    if args.pipeline:
+        classifier = pipeline("zero-shot-classification", model="distilbert-base-uncased")
+        result_info = classifier(generated, labels)
+        score = result_info['scores'][0]
+        result = result_info['labels'][0]
+    else:
+        model_file_path = Path(args.model_path)
+        assert(model_file_path.exists())
+
+        tokenizer_file_path = Path(f"{model_file_path.parent.absolute()}/tokenizer")
+        assert(tokenizer_file_path.exists())
+
+        model_name = model_file_path.parent.name
+
+        tokenizer = AutoTokenizer.from_pretrained(tokenizer_file_path.absolute())
+        # Загрузка модели
+        model = AutoModelForSequenceClassification.from_pretrained(model_file_path.absolute())
+        
+        # Токенизация данных
+        inputs = tokenizer(generated, 
+                           return_tensors='pt', 
+                           padding=True, 
+                           truncation=True, 
+                           max_length=256, 
+                           add_special_tokens = True)
+
+        # Прогон текста через модель
+        with torch.no_grad():  # Отключаем градиенты для оптимизации
+            outputs = model(**inputs)
+
+        # Извлечение предсказаний
+        logits = outputs.logits
+        result_lable = torch.argmax(logits, dim=-1)
+        propabilities = F.softmax(logits, dim=1)
+        score = propabilities[0][result_lable].detach().item()
+        result = idx2labels[result_lable.detach().item()]
+        
+    output_file_path = Path(args.output_path)
+    output_file = Path(f"{output_file_path.absolute()}/{target_label.replace(' ', '-')}_generated_samples.csv")
+    file_create = output_file.exists()
+
+    if result == target_label and score > 0.7:
+        # clean result before insert in file
+        cleaned_generation = generated.replace(',', '').replace('\n', ' ')
+        
+        with open(output_file.absolute(), 'a') as fd:
+            if not file_create:
+                fd.write("Question,label\n")
+            fd.write(f"{cleaned_generation},{result_lable.detach().item()}\n")
+        accepted_samples_number += 1
     
-    # Токенизация данных
-    inputs = tokenizer(generated, return_tensors='pt')
-
-    # Прогон текста через модель
-    with torch.no_grad():  # Отключаем градиенты для оптимизации
-        outputs = model(**inputs)
-
-    # Извлечение предсказаний
-    logits = outputs.logits
-    result_lable = torch.argmax(logits, dim=-1)
-    propabilities = F.softmax(logits, dim=1)
-    score = propabilities[0][result_lable].detach().item()
-    result = idx2labels[result_lable.detach().item()]
-
-print(f"\nClassification: {result} (score={score:.2f})")
-
-if result == target_label and score > 0.7:
-    print("Example accepted")
-else:
-    print("Example declined")
+print("SUCCESSFUL GENERATION COMPLITION.")
+print(f"NUMBER OF ACCEPTED SAMPLES [{accepted_samples_number}/{n_samples}]")
