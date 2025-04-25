0f3a082a4989188d48d22cf8a407883ebf9989d1
Filippenko Pavel <filippenko.ps@phystech.edu>
2025-04-25 20:03:25 +0300

    debug augmentation and generation

--- src/generation/augmentation.py
+++ src/generation/augmentation.py
@@ -1,20 +1,27 @@
 from transformers import pipeline
+import pandas as pd
 
 paraphraser = pipeline("text2text-generation", 
                        model="ramsrigouthamg/t5_paraphraser", 
                        tokenizer="t5-base")
 
-text = r"For an integer $n\geq 3$, let $\theta=2\pi/n$.  Evaluate the determinant of the $n\times n$ matrix $I+A$, where $I$ is the $n\times n$ identity matrix and $A=(a_{jk})$ has entries $a_{jk}=\cos(j\theta+k\theta)$ for all $j,k$."
-prompt = f"paraphrase: {text} </s>"
+prompt = "paraphrase: {text} </s>"
 
-outputs = paraphraser(prompt, 
-                      max_length=128, 
-                      num_return_sequences=3, 
-                      do_sample=True,
-                      temperature=1.1,
-                      top_k=500,
-                      top_p=0.95)
+df = pd.read_csv('../../data/train.csv')
 
-print("Perephrased outputs:")
-for i, out in enumerate(outputs, 1):
-    print(f"{i}) {out['generated_text']}")
+for idx, row in df.iterrows():
+    outputs = paraphraser(prompt.format(text = row['Question']), 
+                          max_length=128, 
+                          num_return_sequences=3, 
+                          do_sample=True,
+                          temperature=1.1,
+                          top_k=500,
+                          top_p=0.95)
+    
+    print(f"Original text {row['Question']}")
+    print("Perephrased outputs:")
+    for i, out in enumerate(outputs, 1):
+        print(f"{i}) {out['generated_text']}")
+    print()
+    
+    if idx == 5: break
--- src/generation/generation.py
+++ src/generation/generation.py
@@ -2,20 +2,39 @@ from vllm import LLM, SamplingParams
 from transformers import pipeline
 import gc
 import torch
+import argparse
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer
+import numpy as np
+import torch.nn.functional as F
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--pipeline', action='store_true', help='use raw pipeline to classify generationed result')
+args = parser.parse_args()
 
 # Загружаем LLM
-# llm = LLM(model='Qwen/Qwen2.5-0.5B-Instruct')
-llm = LLM(model='deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B',
-          tokenizer_mode="auto",
-          dtype="auto",
-          gpu_memory_utilization=0.9,
-          max_num_seqs=128,
-          max_model_len=2048)
+llm = LLM(model='Qwen/Qwen2.5-0.5B-Instruct')
+# llm = LLM(model='deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B',
+#           tokenizer_mode="auto",
+#           dtype="auto",
+#           gpu_memory_utilization=0.9,
+#           max_num_seqs=128,
+#           max_model_len=2048)
 
 target_label = "Linear Algebra"
 labels = ["Algebra", "Geometry and Trigonometry", "Calculus and Analysis",
           "Probability and Statistics", "Number Theory", "Combinatorics and Discrete Math",
           "Linear Algebra", "Abstract Algebra and Topology"]
+
+idx2labels ={
+    0: "Algebra", 
+    1: "Geometry and Trigonometry", 
+    2: "Calculus and Analysis",
+    3: "Probability and Statistics", 
+    4: "Number Theory", 
+    5: "Combinatorics and Discrete Math",
+    6: "Linear Algebra", 
+    7: "Abstract Algebra and Topology"
+}
 
 # Промпт с уточнением стиля
 prompt = f"""Generate an example of a short piece of mathematical text, which is {target_label} problem.
@@ -37,15 +56,37 @@ gc.collect()
 torch.cuda.empty_cache()
 
 # Проверка классификатором
-#! мы можем использовать здесь предобученную нами модель
 #! ради экономии видеопамяти эту часть можно вынести в отдельный скрипт
 #! мы можем попробовать здесь другие модели
-classifier = pipeline("zero-shot-classification", model="distilbert-base-uncased")
-result = classifier(generated, labels)
+if args.pipeline:
+    classifier = pipeline("zero-shot-classification", model="distilbert-base-uncased")
+    result_info = classifier(generated, labels)
+    score = result_info['scores'][0]
+    result = result_info['labels'][0]
+else:
+    tokenizer_file_path = '../../results/bert-base-uncased_results/tokenizer'
+    model_file_path = '../../results/bert-base-uncased_results/checkpoint-5000'
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_file_path)
+    # Загрузка модели
+    model = AutoModelForSequenceClassification.from_pretrained(model_file_path)
+    
+    # Токенизация данных
+    inputs = tokenizer(generated, return_tensors='pt')
 
-print(f"\nClassification: {result['labels'][0]} (score={result['scores'][0]:.2f})")
+    # Прогон текста через модель
+    with torch.no_grad():  # Отключаем градиенты для оптимизации
+        outputs = model(**inputs)
 
-if result['labels'][0] == target_label and result['scores'][0] > 0.7:
+    # Извлечение предсказаний
+    logits = outputs.logits
+    result_lable = torch.argmax(logits, dim=-1)
+    propabilities = F.softmax(logits, dim=1)
+    score = propabilities[0][result_lable].detach().item()
+    result = idx2labels[result_lable.detach().item()]
+
+print(f"\nClassification: {result} (score={score:.2f})")
+
+if result == target_label and score > 0.7:
     print("Example accepted")
 else:
     print("Example declined")
